c01fbf90405ad57875a01b678f87f8e8b20be635
vachito <valen-charlone@hotmail.com>
2023-02-01 11:26:24 -0300

    creando app movies

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import styles from "./MovieCard.module.css";
+import { getMovieImg } from "../utils/getMovieImg";
+
+export function MovieCard({ movie }) {
+  const imageUrl = getMovieImg(movie.poster_path, 300);
+  return (
+    <li className={styles.movieCard}>
+      <Link to={"/movies/" + movie.id}>
+        <img
+          width={300}
+          height={410}
+          className={styles.movieImage}
+          src={imageUrl}
+          alt={movie.title}
+        />
+        <div>{movie.title}</div>
+      </Link>
+    </li>
+  );
+}
